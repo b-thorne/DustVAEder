186e1e52854b12faafe3ccd94ba53e8616c72b73
Ben Thorne <ben.thorne@physics.ox.ac.uk>
2021-02-01 13:38:46 -0800

    Working on gin configuration

--- dustvaeder/vae.py
+++ dustvaeder/vae.py
@@ -8,7 +8,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 import tensorflow_datasets as tfds
-import sonnet as snt
+from tensorflow.keras.initializers import GlorotUniform as glorot_uniform
 
 from pathlib import Path
 
@@ -23,13 +23,14 @@ tfpl = tfp.layers
 
 FLAGS = flags.FLAGS
 
-def ConvBatchLayer(x, filters, kernel_size, strides, activation=tf.nn.relu, momentum=0.9):
-    x = tfkl.Conv2D(filters, kernel_size, strides=strides, padding='same', activation=activation)(x)
-    return tfkl.BatchNormalization(momentum=momentum)(x)
+def ConvBatchLayer(x, filters, kernel_size, strides, activation='relu', padding='same', bn_axis=-1, kernel_initializer=glorot_uniform, momentum=0.9):
+    x = tfkl.Conv2D(filters, kernel_size, strides=strides, padding=padding, activation=None, kernel_initializer=kernel_initializer())(x)
+    x = tfkl.BatchNormalization(momentum=momentum, axis=bn_axis)(x)
+    x = tfkl.Activation(activation)(x)
+    return x
 
-
-def TransConvBatchLayer(x, filters, kernel_size, strides, activation=tf.nn.relu, momentum=0.9):
-    x = tfkl.BatchNormalization(momentum=momentum)(x)
+def TransConvBatchLayer(x, filters, kernel_size, strides, bn_axis=-1, activation=tf.nn.relu, momentum=0.9):
+    x = tfkl.BatchNormalization(momentum=momentum, axis=bn_axis)(x)
     x = tfkl.Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=strides, padding='same', activation=activation)(x)
     return x 
 
@@ -73,6 +74,71 @@ def ConvEncoder(input_shape, num_filters_top=256, kernel_size=4, latent_dimensio
     x = tfpl.IndependentNormal(latent_dimension)(x)
     return tfk.Model(inputs=inputs, outputs=x, name="conv_encoder")
 
+def ResNetIdentityBlock(x, filters, kernel_size):
+  x_shortcut = x
+
+  f1, f2, f3 = filters
+
+  x = ConcBatchLayer(x, f1, 1, 1)
+  x = ConvBatchLayer(x, f2, kernel_size, 1)
+  x = ConvBatchLayer(x, f3, 1, 1)
+  
+  x = tfkl.Add()([x, x_shortcut])
+  x = tfkl.Activation('relu')(x)
+  return x
+
+def ResNetConvBlock(x, filters, kernel_size, s=2):
+    x_shortcut = x
+
+    f1, f2, f3 = filters
+
+    x = ConvBatchLayer(x, f1, 1, s, padding='valid')
+    x = ConvBatchLayer(x, f2, kernel_size, 1, padding='same')
+    x = ConvBatchLayer(x, f3, 1, 1, padding='valid')
+
+    x_shortcut = tfkl.Conv2D(f3, 1, s, padding='valid', kernel_initializer=glorot_uniform(seed=0))(x_shortcut)
+    x_shortcut = tfkl.BatchNormalization()(x_shortcut)
+
+    x = tfkl.Add()([x, x_shortcut])
+    x = tfkl.Activation('relu')(x)
+    return x
+
+def ResNetEncoder(input_shape, latent_dimension):
+    X_input = tfkl.Input(input_shape)
+
+    x = tfkl.Conv2D(64, (1, 1), strides=(2, 2), kernel_initializer=glorot_uniform(seed=0))(x_input)
+    x = tfkl.BatchNormalization()(x)
+    x = tfkl.Activation('relu')(x)
+    x = tfkl.MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = ResNetConvBlock(x, [64, 64, 256], 3, s=1)
+    x = ResNetIdentityBlock(x, [64, 64, 256], 3)
+    x = ResNetIdentityBlock(x, [64, 64, 256], 3)
+
+    x = ResNetConvBlock(x, [128, 128, 512], 3, s=2)
+    x = ResNetIdentityBlock(x, [128, 128, 512], 3)
+    x = ResNetIdentityBlock(x, [128, 128, 512], 3)
+    x = ResNetIdentityBlock(x, [128, 128, 512], 3)
+
+    x = ResNetConvBlock(x, [256, 256, 1024], 3, s=1)
+    x = ResNetIdentityBlock(x, [256, 256, 1024], 3)
+    x = ResNetIdentityBlock(x, [256, 256, 1024], 3)
+    x = ResNetIdentityBlock(x, [256, 256, 1024], 3)
+    x = ResNetIdentityBlock(x, [256, 256, 1024], 3)
+
+    x = ResNetConvBlock(x, [512, 512, 2048], 3, s=2)
+    x = ResNetIdentityBlock(x, [512, 512, 2048], 3)
+    x = ResNetIdentityBlock(x, [512, 512, 2048], 3)
+
+    x = tfkl.AveragePooling2D(pool_size=(2, 2), padding='same')(X)
+
+    x = tfkl.Flatten()(x)
+    x = tfkl.Dense(tfpl.IndependentNormal.params_size(latent_dimension), activation=None)(x) 
+    x = tfpl.IndependentNormal(latent_dimension)(x)
+
+    return tfk.Model(inputs=x_input, outputs=x, name='ResNet50')
+
+
 @gin.configurable("VAE")
 class VAE(tf.keras.Model):
     def __init__(
@@ -95,6 +161,8 @@ class VAE(tf.keras.Model):
 
         if encoder_type == "conv":
             self._encoder = ConvEncoder(input_shape)
+        if encoder_type == "ResNet":
+            self._encoder = ResNetEncoder
 
         if decoder_type == "transconv": 
             self._decoder = TransConvDecoder(latent_dimension, input_shape)
@@ -152,7 +220,7 @@ class VAE(tf.keras.Model):
             'kl_loss': self.kl_loss_tracker.result()
         }
 
-def make_21011181_plots(vae, test_dataset):
+def make_21011181_plots(test_dataset, vae):
     import plots 
     dr = iter(test_dataset)
     pred_true = np.zeros((3, 256, 256, 3))
@@ -168,101 +236,72 @@ def make_21011181_plots(vae, test_dataset):
     fig, ax = plots.make_prior_sample_plot(prior_sample)
     fig.savefig("prior_sample.pdf", bbox_inches="tight")
 
-def Eval(vae, test_dataset):
+@gin.configurable("Dataset")
+def Dataset(label, batch_size):
+    return load_dataset(label, batch_size)
+
+@gin.configurable("eval", denylist=["test_dataset"])
+def Eval(test_dataset, vae):
     eval_results = vae.evaluate(test_dataset)
     return eval_results
 
-def Train(vae, optimizer, train_dataset, val_dataset, saved_model_path):
-    epochs = 20
+@gin.configurable("Train", denylist=["train_dataset", "val_dataset"])
+def Train(train_dataset, val_dataset, vae, optimizer=tfk.optimizers.Adam, epochs=2):
     vae.compile(optimizer=optimizer)
-    print(vae._encoder.summary())
-    print(vae._decoder.summary())
-    history = vae.fit(train_dataset, epochs=epochs, validation_data=val_dataset)
-    vae.save_weights(saved_model_path, save_format='tf')    
+    history = vae.fit(train_dataset, epochs=epochs, validation_data=val_dataset)   
     return vae
 
-def Load(saved_model_path, optimizer, input_shape):
+@gin.configurable("load", denylist=["saved_model_path"])
+def Load(saved_model_path, vae, optimizer=tfk.optimizers.Adam, input_shape=(256, 256, 1)):
     vae = VAE(input_shape=input_shape)
     vae.compile(optimizer=optimizer)
     vae.load_weights(str(saved_model_path)).expect_partial()
     return vae
 
-def vqvae():
-    num_training_updates = 100000
-
-    num_hiddens = 128
-    num_residual_hiddens = 32
-    num_residual_layers = 2
-    # This value is not that important, usually 64 works.
-    # This will not change the capacity in the information-bottleneck.
-    embedding_dim = 64
-
-    # The higher this value, the higher the capacity in the information bottleneck.
-    #num_embeddings = 512
-    num_embeddings = 1024
-
-    # commitment_cost should be set appropriately. It's often useful to try a couple
-    # of values. It mostly depends on the scale of the reconstruction cost
-    # (log p(x|z)). So if the reconstruction cost is 100x higher, the
-    # commitment_cost should also be multiplied with the same amount.
-    commitment_cost = 0.25
-    vq_use_ema = True
-    decay = 0.99    
-    vq_vae = snt.nets.VectorQuantizerEMA(
-        embedding_dim=embedding_dim,
-        num_embeddings=num_embeddings,
-        commitment_cost=commitment_cost,
-        decay=decay)
-    train_data_variance = 1
-    model = VQVAEModel(encoder, decoder, vq_vae, pre_vq_conv1,
-                    data_variance=train_data_variance)
-    optimizer = snt.optimizers.Adam(learning_rate=learning_rate)
-    train_losses = []
-    train_recon_errors = []
-    train_perplexities = []
-    train_vqvae_loss = []
-    return 
-
-
-
 def main(argv):
     del argv # unused
-    BATCH_SIZE = 1
-    input_shape = (256, 256, 1)
-    num_filters_top = 256
-    epochs = FLAGS.epochs
-    beta_1 = 0.5
-    learning_rate = 1e-4
-    beta_disentangle = 5
-    optimizer = tfk.optimizers.Adam(beta_1=beta_1, learning_rate=learning_rate)
+    # parse the configuration file with hyperparameter settings
+    gin.parse_config_file(FLAGS.gin_config)
 
+    # make directory for results to be saved if it doe not exist
     results_dir = Path(FLAGS.results_dir).absolute()
     results_dir.mkdir(exist_ok=True, parents=True)
     saved_model_path = str(results_dir / "vae")
 
-    train_dataset, val_dataset, test_dataset, dataset_info = load_dataset(FLAGS.dataset, batch_size=BATCH_SIZE)
+    # Get dataset
+    train_dataset, val_dataset, test_dataset, dataset_info = Dataset()
 
     if FLAGS.mode == "standard":
-        vae = VAE(input_shape=dataset_info['input_shape'])
-        vae = Train(vae, optimizer, train_dataset, val_dataset, saved_model_path)
-        Eval(vae, test_dataset)
+        # perform training
+        vae = Train(train_dataset, val_dataset)
+        # save trained model
+        vae.save_weights(saved_model_path, save_format='tf')
+        # evaluate trained model 
+        Eval(test_dataset, vae)
 
     if FLAGS.mode == "training":
-        _ = Train(vae, optimizer, train_dataset, val_dataset, saved_model_path)
+        # perform training
+        vae = Train(train_dataset, val_dataset)
+        # save trained model
+        vae.save_weights(saved_model_path, save_format='tf') 
 
     if FLAGS.mode == "eval":
-        vae = Load(saved_model_path, optimizer, dataset_info['input_shape'])
-        eval_results = Eval(vae, test_dataset)
+        # evaluate pre-trained model
+        vae = Load(saved_model_path, gin.REQUIRED)
+        eval_results = Eval(test_dataset, vae)
 
     if FLAGS.mode == "2101.11181":
-        vae = Load(saved_model_path, optimizer, dataset_info['input_shape'])
-        make_21011181_plots(vae, test_dataset)
+        # make plots for paper arXiv:2101.11181
+        # load model
+        vae = Load(saved_model_path, gin.REQUIRED)
+        # make plots
+        make_21011181_plots(test_dataset, vae)
 
 
 if __name__ == "__main__":
-    flags.DEFINE_enum("dataset", "GNILC", ["GNILC", "MHD"], "Which dataset to use")
+    flags.DEFINE_enum("dataset", "GNILC", ["GNILC", "MHD"], "Which dataset to use.")
     flags.DEFINE_enum("mode", "standard", ["standard", "training", "eval", "2101.11181"],  
         "Which mode to run in. Standard will train the model, and run the evaluations.")
     flags.DEFINE_string("results_dir", "/global/cscratch1/sd/bthorne/dustvaeder/results/default", "Directory to be created for results.")
-    flags.DEFINE_integer("epochs", 10, "Number of epochs to train for")
+    flags.DEFINE_string("gin_config", "./configs/config.gin", "File containing the gin configuration.")
     app.run(main)
